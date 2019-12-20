44b7491357a94915eee6f4ce244273e851fcc3b4
teja0104 <komaravolusrujana@gmail.com>
2019-12-20 15:38:34 +0530

    name changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.static('frontend'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
 app.get('/login', (req, res) => res.send('login'))
 app.get('/register', (req, res) => res.send('register'))
-var student={"name" : "being zero","college":"vnrvjiet","regno":"1234567"}
+var student={"name" : "k.srujana","college":"vnrvjiet","regno":"1234567"}
 
 app.get('/data',(req,res)=>res.json(student))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
